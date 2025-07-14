deaf3780ab5ea6d2fce8e01839cdd49daffbce08
Vishal Kumar Jangid <imrvishal28@gmail.com>
2025-07-14 17:01:14 +0000

    Add file system and OS module examples with synchronous and asynchronous operations

---
+++ node/8-os-module.js
@@ -0,0 +1,18 @@
+const { Console } = require("console");
+const os = require("os");
+
+// Info about current user
+const user = os.userInfo();
+console.log(user);
+
+// Method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds.`);
+
+const currentOS = {
+	name: os.type(),
+	release: os.release(),
+	totalMemory: os.totalmem(),
+	freeMemory: os.freemem(),
+};
+
+console.log(currentOS);
